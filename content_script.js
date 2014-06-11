23d11f73d9bbcbc2145cccaf34a20f9712de167c
Victor Tanudjaja <viperfx07@gmail.com>
2014-06-11 22:04:33 +1000

    added indra's script for sofa

--- content_script.js
+++ content_script.js
@@ -1,4 +1,3 @@
-
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
   	var anchors = $('.rs-ad-title > a[itemprop="url"]');
@@ -12,3 +11,35 @@ chrome.runtime.onMessage.addListener(
 	}
 );
 
+
+$(function(){
+	$('#adsense-top').remove();
+	$('#topads-sr-title').remove();
+	$('#adsense-middle').remove();
+	$('#srchrslt-adtable-topads').remove();
+	$('#adsense-bottom').remove();
+
+	$('div[id^="div-gpt-ad-"]').remove();
+	$('#user-activity').remove();
+
+	$('.placeholder-image').parents('li').remove();
+
+	$("span[itemprop='name']").each(function(){
+	    if($(this).html().match(/barber|bean|bed|bench|black|blue|burgundy|cabinet|chaise|chinese|computer|corner|dining|electric|floral|fold|fouton|freedom|fridge|futon|jimmy possum|ikea|lazyboy|l-shaped|massage|moran|office|ottoman|outdoor|pink|pair|plastic|purple|ratan|rattan|recliner|red|rocking|rossini|salmon|scali|set|sofabed|suite|stool|swivel|table|theatre|trailer|tv|two|wicker|2|3|4|5|6|7/i) != null){
+	        $(this).parents('li').remove();
+	    }
+	});
+
+	$(".rs-ad-description").each(function(){
+	    var a = $(this).html();
+	    
+	    if(a.match(/blue|.com.au|ikea|nick scali|red|three-seater|three seater|two seater|2 seater|3 seater/i) != null){
+	        $(this).parents('li').remove();
+	    } else {
+	        var acount = a.trim().replace( /[^\w ]/g, "" ).split( /\s+/ ).length;
+	        if(acount < 7){
+	        	$(this).parents('li').remove();
+	        }
+	    }
+	});
+})
